8f5458e211d35d7ef8baad3537c93c5ad5459503
iludart <peter.penev@hotmail.com>
2018-06-22 15:37:17 +0300

    Hookend stomp test to activemq

--- src/components/common/StompTest.jsx
+++ src/components/common/StompTest.jsx
@@ -4,6 +4,8 @@ import Stomp from 'stompjs';
 export default class StyleTest extends Component {
   constructor(props) {
     super(props);
+
+    this.sendMessage = this.sendMessage.bind(this);
   }
 
   componentWillMount() {
@@ -11,23 +13,40 @@ export default class StyleTest extends Component {
   }
 
   initStomp() {
+    const uuid = '123';
     const url = 'ws://localhost:61614';
-    const login = '';
-    const passcode = '';
-    const destination = '';
-    this.client = Stomp.client(url);
-
-    this.client.connect(login, passcode, function(frame) {
+    const login = 'admin';
+    const passcode = 'admin';
+    const receiveDestination = 'pipilota/' + uuid;
+    const client = Stomp.client(url);
+    this.client = client;
+    client.connect(login, passcode, function (frame) {
       console.log("connected to Stomp");
-      this.client.subscribe(destination, function(message) {
+      client.subscribe(receiveDestination, function (message) {
         console.log(message);
       });
     });
+
+    console.log(client.connected);
+  }
+
+  sendMessage() {
+    const uuid = '123';
+    const msgObject = {
+      uuid: uuid,
+      query: 'region=15664&currency=USD&startDate=23/06/2018&endDate=24/06/2018&rooms=%5B%7B"adults":2,"children":%5B%5D%7D%5D'
+    };
+
+    const msg = JSON.stringify(msgObject);
+
+    const sendDestination = 'pipilota';
+    this.client.send(sendDestination, {}, msg);
   }
 
   render() {
     return (
       <div className="container">
+        <button onClick={this.sendMessage}>SEND</button>
       </div>
     );
   }
