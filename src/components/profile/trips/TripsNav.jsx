0b30727794801c51d9d2aff12223449db5eca006
iludart <peter.penev@hotmail.com>
2018-06-26 18:34:44 +0300

    Fixed my trips local nav

--- src/components/profile/trips/TripsNav.jsx
+++ src/components/profile/trips/TripsNav.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
+import '../../../styles/css/components/profile/admin_panel/navigation-tab.css';
+
 export default function TripsNav() {
   return (
     <div>
-      <ul className="tab-navigation">
+      <ul className="navigation-tab">
         <li><NavLink exact activeClassName="active" to="/profile/trips/hotels"><h2>Hotels</h2></NavLink></li>
         <li><NavLink exact activeClassName="active" to="/profile/trips/homes"><h2>Homes</h2></NavLink></li>
       </ul>
