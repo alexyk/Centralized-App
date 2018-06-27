639dd95bf8204feba32f784f43e29d4dc901e7d2
vonrepiks <ico_skipernov@abv.bg>
2018-06-27 22:26:45 +0300

    Add details feature to hotel trip listings.

--- src/components/profile/trips/TripsRouter.jsx
+++ src/components/profile/trips/TripsRouter.jsx
@@ -4,17 +4,41 @@ import React from 'react';
 import TripsNav from './TripsNav';
 import HotelTripsPage from './HotelTripsPage';
 import HomeTripsPage from './HomeTripsPage';
+import HotelTripDetails from './HotelTripDetails';
 
 export default function TripsRouter() {
+
+  const getHeader = () => {
+    return (
+      <div>
+        <TripsNav />
+        <hr />
+      </div>
+    );
+  };
+
   return (
     <div className="my-reservations">
       <section id="profile-my-reservations">
         <div className="container">
-          <TripsNav />
-          <hr />
           <Switch>
-            <Route exact path="/profile/trips/hotels" render={() => <HotelTripsPage />} />
-            <Route exact path="/profile/trips/homes" render={() => <HomeTripsPage />} />
+            <Route exact path="/profile/trips/hotels" render={() => {
+              return (
+                <div>
+                  {getHeader()}
+                  <HotelTripsPage />
+                </div>
+              );
+            }} />
+            <Route exact path="/profile/trips/homes" render={() => {
+              return (
+                <div>
+                  {getHeader()}
+                  <HomeTripsPage />
+                </div>
+              );
+            }} />
+            <Route exact path="/profile/trips/hotels/:id" render={() => <HotelTripDetails />} />
             <Redirect from="/profile/trips" to="/profile/trips/hotels" />
           </Switch>
         </div>
