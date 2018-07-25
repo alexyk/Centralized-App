4ca48daf26d693f49b618d804ab4bf2c6b4e5342
nikoletaogoryalkova <nikoletaogoryalkova@gmail.com>
2018-07-25 18:34:44 +0300

    Added new component - HomesBookingPage.jsx

---
+++ src/components/homes/booking/HomesBookingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomesBookingPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello HomesBookingPage!
+      </div>
+
+    );
+  }
+}
+
+export default HomesBookingPage;
