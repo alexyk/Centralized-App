808a3e278009a2f41f7c0d813b88527699524984
iludart <peter.penev@hotmail.com>
2018-08-30 16:58:20 +0300

    Changed profile verification constants

--- src/constants/profileVerification.js
+++ src/constants/profileVerification.js
@@ -5,7 +5,7 @@ export const ProfileVerification = {
   LASTNAME_UNVERIFED: 'Last name is missing',
   EMAIL_VERIFIED: 'You have confirmed your email. A confirmed email is important to allow us to securely communicate with you',
   EMAIL_UNVERIFIED: 'Email is missing',
-  PHONENUMBER_VERIFIED: 'Your number is only shared with another Airbnb member once you have confirmed booking',
+  PHONENUMBER_VERIFIED: 'Your number is only shared with another LockTrip members once you have confirmed a booking',
   PHONENUMBER_UNVERIFIED: 'Phone number is missing',
   COUNTRY_VERIFIED: 'Country is existing',
   COUNTRY_UNVERIFIED: 'Country is missing',
