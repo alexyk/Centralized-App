bdc464ba1956077df2fa69bcfda92529655755b2
RoYaLBG <royal4o@gmail.com>
2018-09-05 13:24:07 +0300

    adding incomplete status

--- src/components/profile/airdrop/AirdropPage.jsx
+++ src/components/profile/airdrop/AirdropPage.jsx
@@ -54,6 +54,7 @@ class AirdropPage extends Component {
     this._getVerifiedStatus = this._getVerifiedStatus.bind(this);
     this._getUnverifiedStatus = this._getUnverifiedStatus.bind(this);
     this._getFailedStatus = this._getFailedStatus.bind(this);
+    this._getIncompleteStatus = this._getIncompleteStatus.bind(this);
   }
 
   componentWillMount() {
@@ -279,6 +280,8 @@ class AirdropPage extends Component {
        return this._getVerifiedStatus();
      case 'FAILED':
        return this._getFailedStatus();
+     case 'INCOMPLETE':
+       return this._getIncompleteStatus();
      default:
        return this._getUnverifiedStatus();
    }
@@ -290,7 +293,7 @@ class AirdropPage extends Component {
         <span className="step-check checked"  style={{"margin-top": "-0.4em"}}></span>
         <span className="emphasized-text">Verified Balance</span>
       </div>,
-      <div className="balance-row__content">$10</div>
+      <div className="balance-row__content">${Math.max(10, this.props.airdropInfo.referralCount * 5 + 10)}</div>
     ];
   }
 
@@ -305,7 +308,21 @@ class AirdropPage extends Component {
     return [
       <div className="balance-row__label">
         <span className="step-check unchecked" style={{"margin-top": "1.5em"}}></span>
-        <span className="mandatory">Duplicate accounting/multi accounting has been detected. As a result your balance has been voided.</span>
+        <span className="mandatory">
+          Duplicate accounting/multi accounting has been detected. As a result your balance has been voided.
+        </span>
+      </div>,
+      <div className="balance-row__content centered-balance">$0</div>
+    ];
+  }
+
+  _getIncompleteStatus() {
+    return [
+      <div className="balance-row__label">
+        <span className="step-check unchecked" style={{"margin-top": "1.5em"}}></span>
+        <span className="mandatory">
+          Our checks indicate that you have not completed all social joins. Please make sure you have joined our telegram, followed us on Twitter and Facebook.
+        </span>
       </div>,
       <div className="balance-row__content centered-balance">$0</div>
     ];
