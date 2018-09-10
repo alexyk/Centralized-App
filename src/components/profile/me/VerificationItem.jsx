2a9d5c2ef4216152fc557394e77a9ea2dcf56e08
iludart <peter.penev@hotmail.com>
2018-09-10 15:56:05 +0300

    Fixed broken verification items

--- src/components/profile/me/VerificationItem.jsx
+++ src/components/profile/me/VerificationItem.jsx
@@ -10,14 +10,14 @@ function VerificationItem(props) {
   const { item, verified } = props;
   const sufix = verified === true ? '_VERIFIED' : '_UNVERIFIED';
   return (
-    <div className="box">
-      <div className="left-part">
+    <div className="verification-item">
+      <div>
         <h2>{_.startCase(_.camelCase(item))}</h2>
         <h5>{ProfileVerification[item.toUpperCase() + sufix]}</h5>
       </div>
-      <div className="right-part">
+      {/* <div className="right-part">
         {verified === true ? null : <Link to="/profile/me/edit" className="button">Provide</Link>}
-      </div>
+      </div> */}
     </div>
   );
 }
