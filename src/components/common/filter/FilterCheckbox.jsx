023ba8e4bbeb29c6f3c7717218447d3a21976138
iludart <peter.penev@hotmail.com>
2018-09-19 10:49:21 +0300

    LOC-761 Modified checkbox styling

--- src/components/common/filter/FilterCheckbox.jsx
+++ src/components/common/filter/FilterCheckbox.jsx
@@ -2,22 +2,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 // import '../../../styles/css/components/profile/listings/listing-facilities.css';
 
-const FilterCheckbox = (props) => (
+const FilterCheckbox = ({ id, text, checked }) => (
   <div className="key">
-    <input type="checkbox" name={props.id} checked={props.checked} />
-    <label htmlFor={props.id}>{props.text}</label>
-    {/* <label className={`filter-check-label${props.checked ? ' active' : ''}`}>
-      <span className="filter-check"></span>
-      <span className="filter-check-text">{props.text}</span>
-      <span className="filter-check-count">{props.count}</span>
-    </label> */}
+    <input type="checkbox" name={id} checked={checked} />
+    <label htmlFor={id}>{text}</label>
   </div>
 );
 
 FilterCheckbox.propTypes = {
-  checked: PropTypes.bool,
+  id: PropTypes.string,
   text: PropTypes.string,
-  count: PropTypes.number
+  checked: PropTypes.bool,
 };
 
 export default FilterCheckbox;
