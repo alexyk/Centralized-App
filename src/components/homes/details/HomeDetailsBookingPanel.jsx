99189c248fae3f9768893e955a33c671c184c8ab
ChrisPam <chrispam1509@gmail.com>
2018-10-04 15:24:47 +0300

    Fixed issues after review

--- src/components/homes/details/HomeDetailsBookingPanel.jsx
+++ src/components/homes/details/HomeDetailsBookingPanel.jsx
@@ -9,129 +9,145 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { isInvalidRange } from '../common/detailsPageUtils.js';
+import PropTypes from 'prop-types';
 
 class HomeDetailsBookingPanel extends React.Component {
-    render() {
-        const { rates } = this.props.currenciesRatesInfo;
-        if (rates === null || !this.props.calendar) {
-            return <div className="loader"></div>
-        }
-
-        const {
-            calendar,
-            nights,
-            startDate,
-            currencyCode,
-            cleaningFee,
-        } = this.props;
-
-        const { currency,
-            currencySign } = this.props.paymentInfo;
-
-        function getPriceForPeriod(startDate, nights, calendar) {
-            let price = 0;
-
-            let startDateIndex = calendar.findIndex(x => x.date === startDate.format('DD/MM/YYYY'));
-            if(startDateIndex && startDateIndex < 0) {
-                return 0;
-            }
-            for (let i = startDateIndex; i < nights + startDateIndex; i++) {
-                price += calendar[i].price
-            }
-            if (nights === 0) {
-                return 0;
-            }
-            return price / nights;
-        }
-        debugger;
-        const price = getPriceForPeriod(startDate, nights, calendar);
-
-        let defaultDailyPrice = CurrencyConverter.convert(rates, currencyCode, currency, price);
-        let fiatPriceInRoomsXMLCurrency = CurrencyConverter.convert(rates, currencyCode, RoomsXMLCurrency.get(), price);
-
-        let defaultCleaningFee = CurrencyConverter.convert(rates, currencyCode, currency, cleaningFee);
-        let fiatCleaningFeePriceInRoomsXMLCurrency = CurrencyConverter.convert(rates, currencyCode, RoomsXMLCurrency.get(), cleaningFee);
-
-        const invalidRange = isInvalidRange(startDate, nights, calendar);
-
-        if (invalidRange === true) {
-            defaultDailyPrice = 0;
-            fiatPriceInRoomsXMLCurrency = 0;
-            defaultCleaningFee = 0;
-            fiatCleaningFeePriceInRoomsXMLCurrency = 0;
-        }
-
-        return (<div className="home-booking-panel">
-            <div className="box" id="test">
-                <p className="default-price"><span className="main-fiat">{currencySign}{defaultDailyPrice.toFixed(3)}</span> <LocPrice fiat={fiatPriceInRoomsXMLCurrency} /> /per night</p>
-                <div className="booking-dates">
-                    <div className="datepicker">
-                        <DatePickerPreview
-                            startDate={this.props.startDate}
-                            endDate={this.props.endDate}
-                            handleChangeStart={this.props.handleChangeStart}
-                            handleChangeEnd={this.props.handleChangeEnd}
-                            calendar={this.props.calendar}
-                        />
-                    </div>
-                    <div className="days-of-stay">
-                        <span className="icon-moon"></span>
-                        <span>{this.props.nights} nights</span>
-                    </div>
-                </div>
-                <div className="booking-guests">
-                    <select>
-                        {this.props.guestArray.map((item, i) => {
-                            return <option key={i}>{`${item} Guests`}</option>
-                        })}
-                    </select>
-                </div>
-                <div className="fiat-price-box">
-                    <div className="without-fees">
-                        <p>{currencySign}{defaultDailyPrice.toFixed(3)}  x {this.props.nights} nights</p>
-                        <p>{currencySign}{(defaultDailyPrice * this.props.nights).toFixed(3)}</p>
-                    </div>
-                    <div className="cleaning-fee">
-                        <p>Cleaning fee</p>
-                        <p>{currencySign}{defaultCleaningFee.toFixed(3)}</p>
-                    </div>
-                    <div className="total">
-                        <p>Total</p>
-                        <p>{currencySign}{((defaultDailyPrice * this.props.nights) + defaultCleaningFee).toFixed(3)}</p>
-                    </div>
-                </div>
-                {/* <button className="pay-in">Request Booking in FIAT</button> */}
-                <hr />
-                <div className="loc-price-box">
-                    <div className="without-fees">
-                        <p><LocPrice fiat={fiatPriceInRoomsXMLCurrency} brackets={false} /> x {this.props.nights} nights</p>
-                        <p><LocPrice fiat={fiatPriceInRoomsXMLCurrency * this.props.nights} brackets={false} /></p>
-                    </div>
-                    <div className="cleaning-fee">
-                        <p>Cleaning fee</p>
-                        <p><LocPrice fiat={fiatCleaningFeePriceInRoomsXMLCurrency} brackets={false} /></p>
-                    </div>
-                    <div className="total">
-                        <p>Total</p>
-                        <p><LocPrice fiat={(fiatPriceInRoomsXMLCurrency * this.props.nights) + fiatCleaningFeePriceInRoomsXMLCurrency} brackets={false} /></p>
-                    </div>
-                </div>
-                {this.props.userInfo.isLogged ?
-                    <Link to={`/homes/listings/book/${this.props.match.params.id}${this.props.location.search}`} onClick={e => invalidRange && e.preventDefault()} className={[invalidRange ? 'disabled' : null, "pay-in"].join(' ')}>Request Booking in LOC</Link> :
-                    <button className="pay-in" onClick={(e) => this.props.dispatch(openModal(LOGIN, e))}>Login</button>}
-                <p className="booking-helper">You won't be charged yet</p>
-            </div>
-        </div>)
+  render() {
+    const { currencyExchangeRates } = this.props.exchangeRatesInfo;
+    if (!currencyExchangeRates || !this.props.calendar) {
+      return <div className="loader"></div>
     }
+
+    const {
+      calendar,
+      nights,
+      startDate,
+      currencyCode,
+      cleaningFee,
+    } = this.props;
+
+    const { currency,
+      currencySign } = this.props.paymentInfo;
+
+    function getPriceForPeriod(startDate, nights, calendar) {
+      let price = 0;
+
+      let startDateIndex = calendar.findIndex(x => x.date === startDate.format('DD/MM/YYYY'));
+      if (startDateIndex && startDateIndex < 0) {
+        return 0;
+      }
+      for (let i = startDateIndex; i < nights + startDateIndex; i++) {
+        price += calendar[i].price
+      }
+      if (nights === 0) {
+        return 0;
+      }
+      return price / nights;
+    }
+    const price = getPriceForPeriod(startDate, nights, calendar);
+
+    let defaultDailyPrice = CurrencyConverter.convert(currencyExchangeRates, currencyCode, currency, price);
+    let fiatPriceInRoomsXMLCurrency = CurrencyConverter.convert(currencyExchangeRates, currencyCode, RoomsXMLCurrency.get(), price);
+
+    let defaultCleaningFee = CurrencyConverter.convert(currencyExchangeRates, currencyCode, currency, cleaningFee);
+    let fiatCleaningFeePriceInRoomsXMLCurrency = CurrencyConverter.convert(currencyExchangeRates, currencyCode, RoomsXMLCurrency.get(), cleaningFee);
+
+    const invalidRange = isInvalidRange(startDate, nights, calendar);
+
+    if (invalidRange === true) {
+      defaultDailyPrice = 0;
+      fiatPriceInRoomsXMLCurrency = 0;
+      defaultCleaningFee = 0;
+      fiatCleaningFeePriceInRoomsXMLCurrency = 0;
+    }
+
+    return (<div className="home-booking-panel">
+      <div className="box" id="test">
+        <p className="default-price"><span className="main-fiat">{currencySign}{defaultDailyPrice.toFixed(3)}</span> <LocPrice fiat={fiatPriceInRoomsXMLCurrency} /> /per night</p>
+        <div className="booking-dates">
+          <div className="datepicker">
+            <DatePickerPreview
+              startDate={this.props.startDate}
+              endDate={this.props.endDate}
+              handleChangeStart={this.props.handleChangeStart}
+              handleChangeEnd={this.props.handleChangeEnd}
+              calendar={this.props.calendar}
+            />
+          </div>
+          <div className="days-of-stay">
+            <span className="icon-moon"></span>
+            <span>{this.props.nights} nights</span>
+          </div>
+        </div>
+        <div className="booking-guests">
+          <select>
+            {this.props.guestArray.map((item, i) => {
+              return <option key={i}>{`${item} Guests`}</option>
+            })}
+          </select>
+        </div>
+        <div className="fiat-price-box">
+          <div className="without-fees">
+            <p>{currencySign}{defaultDailyPrice.toFixed(3)}  x {this.props.nights} nights</p>
+            <p>{currencySign}{(defaultDailyPrice * this.props.nights).toFixed(3)}</p>
+          </div>
+          <div className="cleaning-fee">
+            <p>Cleaning fee</p>
+            <p>{currencySign}{defaultCleaningFee.toFixed(3)}</p>
+          </div>
+          <div className="total">
+            <p>Total</p>
+            <p>{currencySign}{((defaultDailyPrice * this.props.nights) + defaultCleaningFee).toFixed(3)}</p>
+          </div>
+        </div>
+        {/* <button className="pay-in">Request Booking in FIAT</button> */}
+        <hr />
+        <div className="loc-price-box">
+          <div className="without-fees">
+            <p><LocPrice fiat={fiatPriceInRoomsXMLCurrency} brackets={false} /> x {this.props.nights} nights</p>
+            <p><LocPrice fiat={fiatPriceInRoomsXMLCurrency * this.props.nights} brackets={false} /></p>
+          </div>
+          <div className="cleaning-fee">
+            <p>Cleaning fee</p>
+            <p><LocPrice fiat={fiatCleaningFeePriceInRoomsXMLCurrency} brackets={false} /></p>
+          </div>
+          <div className="total">
+            <p>Total</p>
+            <p><LocPrice fiat={(fiatPriceInRoomsXMLCurrency * this.props.nights) + fiatCleaningFeePriceInRoomsXMLCurrency} brackets={false} /></p>
+          </div>
+        </div>
+        {this.props.userInfo.isLogged ?
+          <Link to={`/homes/listings/book/${this.props.match.params.id}${this.props.location.search}`} onClick={e => invalidRange && e.preventDefault()} className={[invalidRange ? 'disabled' : null, "pay-in"].join(' ')}>Request Booking in LOC</Link> :
+          <button className="pay-in" onClick={(e) => this.props.dispatch(openModal(LOGIN, e))}>Login</button>}
+        <p className="booking-helper">You won't be charged yet</p>
+      </div>
+    </div>)
+  }
 }
 
+HomeDetailsBookingPanel.propTypes = {
+  exchangeRatesInfo: PropTypes.object,
+  userInfo: PropTypes.object,
+  paymentInfo: PropTypes.object,
+  dispatch: PropTypes.func,
+  calendar: PropTypes.array,
+  nights: PropTypes.number,
+  startDate: PropTypes.any,
+  endDate: PropTypes.any,
+  currencyCode: PropTypes.string,
+  cleaningFee: PropTypes.number,
+  handleChangeStart: PropTypes.func,
+  handleChangeEnd: PropTypes.func,
+  guestArray: PropTypes.array
+};
+
 function mapStateToProps(state) {
-    const { paymentInfo, currenciesRatesInfo, userInfo } = state;
-    return {
-        paymentInfo,
-        currenciesRatesInfo,
-        userInfo
-    };
+  const { paymentInfo, exchangeRatesInfo, userInfo } = state;
+  return {
+    paymentInfo,
+    exchangeRatesInfo,
+    userInfo
+  };
 }
 
 export default withRouter(connect(mapStateToProps)(HomeDetailsBookingPanel));
--- src/components/homes/details/HomeDetailsRatingBox.jsx
+++ src/components/homes/details/HomeDetailsRatingBox.jsx
@@ -2,63 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function HomeDetailsRatingBox(props) {
-    const getRatingString = (ratingNumber, reviewsCount) => {
+  const getRatingString = (ratingNumber, reviewsCount) => {
 
-        let result = '';
-        let ratingRoundedNumber = Math.round(ratingNumber);
+    let result = '';
+    let ratingRoundedNumber = Math.round(ratingNumber);
 
-        if (ratingRoundedNumber === 5) {
-            result += 'Excellent';
-        }
-        else if (ratingRoundedNumber === 4) {
-            result += 'Very good';
-        }
-        else if (ratingRoundedNumber === 3) {
-            result += 'Good';
-        }
-        else if (ratingRoundedNumber === 2) {
-            result += 'Sufficient';
-        }
-        else if (ratingRoundedNumber === 1) {
-            result += 'Weak';
-        }
-        else if (ratingRoundedNumber === 0 && reviewsCount !== 0) {
-            result += 'Poor';
-        }
-        result += ' ';
-        result += Math.round(ratingNumber * 100) / 100;
-        return result;
-    };
+    if (ratingRoundedNumber === 5) {
+      result += 'Excellent';
+    }
+    else if (ratingRoundedNumber === 4) {
+      result += 'Very good';
+    }
+    else if (ratingRoundedNumber === 3) {
+      result += 'Good';
+    }
+    else if (ratingRoundedNumber === 2) {
+      result += 'Sufficient';
+    }
+    else if (ratingRoundedNumber === 1) {
+      result += 'Weak';
+    }
+    else if (ratingRoundedNumber === 0 && reviewsCount !== 0) {
+      result += 'Poor';
+    }
+    result += ' ';
+    result += Math.round(ratingNumber * 100) / 100;
+    return result;
+  };
 
-    const calculateStars = (ratingNumber) => {
-        let starsElements = [];
-        let rating = Math.round(ratingNumber);
-        for (let i = 0; i < rating; i++) {
-            starsElements.push(<span key={i} className="fa fa-star full"></span>);
-        }
-        for (let i = 0; i < 5 - rating; i++) {
-            starsElements.push(<span key={100 - i} className="fa fa-star"></span>);
-        }
+  const calculateStars = (ratingNumber) => {
+    let starsElements = [];
+    let rating = Math.round(ratingNumber);
+    for (let i = 0; i < rating; i++) {
+      starsElements.push(<span key={i} className="fa fa-star full"></span>);
+    }
+    for (let i = 0; i < 5 - rating; i++) {
+      starsElements.push(<span key={100 - i} className="fa fa-star"></span>);
+    }
 
-        return starsElements;
-    };
+    return starsElements;
+  };
 
-    return (
-        <div className="home-rating">
-            <p className="home-rating-count">
-                {getRatingString(props.rating, props.reviewsCount)}<span className="max-rating">/5</span>
-                <span className="home-rating-stars">{calculateStars(props.rating)}</span>
-            </p>
+  return (
+    <div className="home-rating">
+      <p className="home-rating-count">
+        {getRatingString(props.rating, props.reviewsCount)}<span className="max-rating">/5</span>
+        <span className="home-rating-stars">{calculateStars(props.rating)}</span>
+      </p>
 
-            {props.reviewsCount >= 0 && <p className="home-rating-reviews">{props.reviewsCount} {props.reviewsCount === 1 ? 'Review' : 'Reviews'}</p>}
-        </div>
-    );
+      {props.reviewsCount >= 0 && <p className="home-rating-reviews">{props.reviewsCount} {props.reviewsCount === 1 ? 'Review' : 'Reviews'}</p>}
+    </div>
+  );
 }
 
 HomeDetailsRatingBox.propTypes = {
-    rating: PropTypes.number,
-    reviewsCount: PropTypes.number,
-    isHomePage: PropTypes.bool
+  rating: PropTypes.number,
+  reviewsCount: PropTypes.number,
+  isHomePage: PropTypes.bool
 };
 
 export default HomeDetailsRatingBox;
