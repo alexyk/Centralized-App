7031b2f44419a36137eb68099c111d429398b36a
vonrepiks <ico_skipernov@abv.bg>
2018-10-05 10:06:35 +0300

    sync loc price rendering with exchanger socket push in page with timer.

--- src/services/socket/locPriceWebSocket.js
+++ src/services/socket/locPriceWebSocket.js
@@ -2,6 +2,7 @@ import store from '../../reduxStore';
 import { updateLocAmounts, clearLocAmounts } from '../../actions/locAmountsInfo';
 import { setLocPriceWebsocketConnection } from '../../actions/exchangerSocketInfo';
 import WS from './exchangerWebsocket';
+import { setSeconds } from '../../actions/locPriceUpdateTimerInfo';
 
 const DEFAULT_SOCKET_METHOD = 'getLocPrice';
 
@@ -26,6 +27,10 @@ class LocPriceWS extends WS {
   handleRecieveMessage(event) {
     if (event) {
       const data = JSON.parse(event.data);
+      if (data.params && data.params.secondsLeft) {
+        const seconds = Math.round(data.params.secondsLeft / 1000);
+        store.dispatch(setSeconds(seconds));
+      }
       store.dispatch(updateLocAmounts(data.id, data.params, data.error));
     }
   }
