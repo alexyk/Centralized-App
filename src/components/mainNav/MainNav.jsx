68e86b7d9782aec86ea464ce7415bdc37ad36022
Georgi Georgiev <ctapnec@icloud.com>
2019-01-16 15:33:30 +0200

    Syntax error fixed. Support link fixed.

--- src/components/mainNav/MainNav.jsx
+++ src/components/mainNav/MainNav.jsx
@@ -131,7 +131,7 @@ class MainNav extends React.Component {
                   FAQ
                 </a>
                 <a
-                  href="https://locktrip.zendesk.com/hc/en-us"
+                  href="https://locktrip.zendesk.com/hc/en-us/requests/new"
                   target="_blank"
                   rel="noopener noreferrer"
                   class="list-menu-item"
@@ -188,9 +188,9 @@ class MainNav extends React.Component {
                   </Link>
                 </DropdownMenu>
               </ListMenu>
-              : <ListMenu>
+              ) : ( <ListMenu>
                 <a href="https://locktrip.zendesk.com/hc/en-us" target="_blank" rel="noopener noreferrer" class="list-menu-item">FAQ</a>
-                <a href="https://locktrip.zendesk.com/hc/en-us" target="_blank" rel="noopener noreferrer" class="list-menu-item">Support</a>
+                <a href="https://locktrip.zendesk.com/hc/en-us/requests/new" target="_blank" rel="noopener noreferrer" class="list-menu-item">Support</a>
                 <div className="list-menu-item" onClick={() => { this.openModal(LOGIN); }}>Login</div>
                 <div className="list-menu-item" onClick={() => { this.openModal(REGISTER); }}>Register</div>
               </ListMenu>
