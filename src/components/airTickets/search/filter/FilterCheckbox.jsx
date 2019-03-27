de5ab32b54266f0c2b47cd07cc693524004ef553
ivanhristov92 <ivan_h92@mail.bg>
2019-03-27 11:49:13 +0200

    Merge branch 'dev' of https://github.com/LockTrip/Centralized-App into dev-fligts-ordering-segments
    # Conflicts:
    #	src/components/airTickets/book/details/AirTicketsDetailsInfoSection.jsx
    ``

--- src/components/airTickets/search/filter/FilterCheckbox.jsx
+++ src/components/airTickets/search/filter/FilterCheckbox.jsx
@@ -1,14 +1,47 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 
 
 class FilterCheckbox extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
 
+  onChange(name, value) {
+    this.setState({
+      [name]: value
+    }, () => { this.props.applyFilters(this.state)});
+  }
+
+  render() {
+      const { stops } = this.props;
+      let items = [];
+
+      {stops.length > 0 &&
+        stops.map(item => {
+          items.push(
+            <Fragment>
+              <label htmlFor={item.changesName}>
+                  <input type="checkbox" value={item.changesId} id={item.changesName} />
+                  <span className="stop-name">{item.changesName}</span>
+              </label>
+            </Fragment>
+          );
+
+        });
+      }
+
+      return (
+        {items}
+      );
+  }
 }
 
 
 FilterCheckbox.propTypes = {
-  results: PropTypes.object
+  stops: PropTypes.object,
+  applyFilters: PropTypes.func
 };
 
-export default AirTicketsSearchFilterPanel;
+export default FilterCheckbox;
