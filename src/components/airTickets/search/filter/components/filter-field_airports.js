66a3478379941923c68e58c773eac675722e540f
ivanhristov92 <ivan_h92@mail.bg>
2019-04-10 13:30:52 +0300

    cleaning up

--- src/components/airTickets/search/filter/components/filter-field_airports.js
+++ src/components/airTickets/search/filter/components/filter-field_airports.js
@@ -2,53 +2,85 @@ import React from "react";
 import * as _ from "ramda";
 
 export default class AirportsFilter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = [];
+    this.onSelectedAirportsChange = this.onSelectedAirportsChange.bind(this);
+    this._renderAirportOptionsForCity = this._renderAirportOptionsForCity.bind(
+      this
+    );
+    this._renderIndividualAirportOption = this._renderIndividualAirportOption.bind(
+      this
+    );
+    this._leaveOnlyAirportsOfSelectedCities = this._leaveOnlyAirportsOfSelectedCities.bind(
+      this
+    );
+  }
+
   render() {
     return (
       <div className="filter-box">
         <div className="filter stops-filter">
           <h5>Airports</h5>
-          {this.renderAirportOptions()}
+          {this._renderAirportOptions()}
         </div>
       </div>
     );
   }
-
-  renderAirportOptions() {
+  _renderAirportOptions() {
     let allAirports = Object.values(this.props.filterOptions.airports.all);
     let airportsByCity = _.groupBy(_.prop("city"), allAirports);
-
     let cityNames = _.keys(airportsByCity);
-
     return cityNames.map((cityName, i) => {
-      let currentCityAirports = airportsByCity[cityName];
-      if (currentCityAirports.length <= 1) {
-        return null;
-      }
-
-      let currentCityAirportsElements = currentCityAirports.map(airport => {
-        return (
-          <li key={airport.airportId}>
-            <label className="filter-label">
-              <input
-                data-testid={"stop-checkbox"}
-                type="checkbox"
-                className="filter-checkbox"
-                name="stops[]"
-                value={airport.airportId}
-                onChange={this.props.handleSelectedAirportsChange}
-              />
-              <span>{airport.airportName}</span>
-            </label>
-          </li>
-        );
-      });
-
-      return (
-        <ul key={cityName}>
-          <h6>{cityName}</h6>
-          {currentCityAirportsElements}
-        </ul>
-      );
+      return this._renderAirportOptionsForCity(cityName, airportsByCity);
     });
+  }
+
+  _renderAirportOptionsForCity(cityName, airportsByCity) {
+    let currentCityAirports = airportsByCity[cityName];
+    if (currentCityAirports.length <= 1) {
+      return null;
+    }
+    let currentCityAirportsElements = currentCityAirports.map(
+      this._renderIndividualAirportOption
+    );
+    return (
+      <ul key={cityName}>
+        <h6>{cityName}</h6>
+        {currentCityAirportsElements}
+      </ul>
+    );
+  }
+
+  _renderIndividualAirportOption(airport) {
+    return (
+      <li key={airport.airportId}>
+        <label className="filter-label">
+          <input
+            data-testid={"stop-checkbox"}
+            type="checkbox"
+            className="filter-checkbox"
+            name="stops[]"
+            value={airport.airportId}
+            onChange={this.onSelectedAirportsChange}
+          />
+          <span>{airport.airportName}</span>
+        </label>
+      </li>
+    );
+  }
+
+  onSelectedAirportsChange(value) {
+    this.props.handleSelectedAirportsChange(value);
+  }
+
+  _leaveOnlyAirportsOfSelectedCities(_filters) {
+    let selectedCities = _filters.airports.all
+      .filter(airport => airport.selected !== undefined)
+      .map(_.prop("city"));
+    return _filters.airports.all.filter(
+      airport => selectedCities.indexOf(airport.city) !== -1
+    );
   }
 }
